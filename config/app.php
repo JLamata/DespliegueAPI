ce66a7a95c33935ae0ce709061fc6946256cf43f
Judit Lamata <mellamojudit@gmail.com>
2025-03-06 09:54:20 +0100

    Rutas conseguidas +config/app.php -> providers

--- config/app.php
+++ config/app.php
@@ -2,6 +2,42 @@
 
 return [
 
+
+        // Otros valores de configuración...
+    
+        'providers' => [
+    
+            /*
+             * Laravel Framework Service Providers...
+             */
+            Illuminate\Auth\AuthServiceProvider::class,
+            Illuminate\Broadcasting\BroadcastServiceProvider::class,
+            Illuminate\Bus\BusServiceProvider::class,
+            Illuminate\Cache\CacheServiceProvider::class,
+           
+            Illuminate\Encryption\EncryptionServiceProvider::class,
+            Illuminate\Filesystem\FilesystemServiceProvider::class,
+            Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
+            Illuminate\Notifications\NotificationServiceProvider::class,
+            Illuminate\Pagination\PaginationServiceProvider::class,
+            Illuminate\Pipeline\PipelineServiceProvider::class,
+            Illuminate\Queue\QueueServiceProvider::class,
+            Illuminate\Redis\RedisServiceProvider::class,
+            Illuminate\Session\SessionServiceProvider::class,
+            Illuminate\Translation\TranslationServiceProvider::class,
+            Illuminate\Validation\ValidationServiceProvider::class,
+            Illuminate\View\ViewServiceProvider::class,
+    
+            /*
+             * Application Service Providers...
+             */
+            App\Providers\AppServiceProvider::class,
+          
+            App\Providers\RouteServiceProvider::class,  // Aquí se incluye el RouteServiceProvider
+    
+        ],
+    
+   
     /*
     |--------------------------------------------------------------------------
     | Application Name
